af2887cc5e0ecf20a3ec8c102cd8f4432299ed54
Babadzakich <bogdamirkobz@gmail.com>
2025-06-06 19:05:52 +0300

    add lifespan

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,24 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 from fastapi import FastAPI
 import uvicorn
 
 from core.config import settings
+from core.database import db_session
 
 from modules.auth import router as auth_router
 from modules.tasks import router as tasks_router
 
-main_app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # startup
+    yield
+    # shutdown
+    print("dispose engine")
+    await db_session.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
 main_app.include_router(auth_router, prefix=settings.api.auth_prefix)
 main_app.include_router(tasks_router, prefix=settings.api.tasks_prefix)
 
