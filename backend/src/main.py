3d49e539ce95bf44d1a293a7afd1c6b6b2e9b6ab
Babadzakich <bogdamirkobz@gmail.com>
2025-06-08 13:52:59 +0300

    included routers

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,7 +8,10 @@ from core.database import db_session
 from core.database import Base
 
 from modules.auth import router as auth_router
+from modules.users import router as users_router
+from modules.groups import router as groups_router
 from modules.tasks import router as tasks_router
+from modules.projects import router as projects_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
@@ -23,7 +26,10 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 main_app = FastAPI(lifespan=lifespan)
 main_app.include_router(auth_router, prefix=settings.api.auth)
-main_app.include_router(tasks_router, prefix=settings.api.tasks)
+main_app.include_router(users_router, prefix=settings.api.users, tags=["Users"])
+main_app.include_router(groups_router, prefix=settings.api.groups, tags=["Groups"])
+main_app.include_router(tasks_router, prefix=settings.api.tasks, tags=["Tasks"])
+main_app.include_router(projects_router, prefix=settings.api.projects, tags=["Projects"])
 
 if __name__ == "__main__":
     uvicorn.run("main:main_app", 
